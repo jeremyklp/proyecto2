4de0af46c51089893e85a195aef676e22918fd55
jorgeheag <herreraajorgeh@gmail.com>
2022-07-25 12:27:58 -0500

    herokujorge

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const helmet = require('helmet')
+const compression = require('compresion');
 
+
+//Routes
+const {cartRouter}= require('./Routes/cart.routes');
+const {productRouter}= require('./Routes/products.route');
+const {userRouter}= require('./Routes/users.route');
+
+//GLOBAL ERROR
 const {globalErrorHandler} = require('./controllers/error.controller');
 
+//utils
 const {AppError} = require('./Utils/app.error');
 
+//Init express app
 const app = express();
 
 // Enable incoming JSON data
@@ -11,12 +22,21 @@ app.use(express.json());
 
 
 //Endpoints
-/*
-app.use('/api/v1/restaurants',restaurantRouter);
-app.use('/api/v1/meals',mealRoute);
-app.use('/api/v1/orders',orderRoute);
 app.use('/api/v1/users',userRouter);
-*/
+app.use('/api/v1/products',productRouter);
+app.use('/api/v1/cart',cartRouter);
+
+
+//Add security headers
+app.use(helmet());
+
+//compress response
+app.use(compression());
+
+//log incoming requests
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+else app.use(morgan('combined'));
+
 app.use('/');
 
 app.all('*', (req, res, next) => {
