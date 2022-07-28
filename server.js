1cdf2510c4f7e51b8316af2609fcd51225585f9d
jeremy <jeremyvallejo2016@gmail.com>
2022-07-27 23:48:03 -0500

    haber si coge

--- server.js
+++ server.js
@@ -15,18 +15,18 @@ dotenv.config({ path: './config.env' });
 initModels();
 // Database authenticated
 db
-	.authenticate()
-	.then(() => console.log('Database authenticated'))
-	.catch(err => console.log(err));
+    .authenticate()
+    .then(() => console.log('Database authenticated'))
+    .catch(err => console.log(err));
 
-// Database synced with models' relations  
+// Database synced with models' relations
 db
-	.sync()
-	.then(() => console.log('Database synced'))
-	.catch(err => console.log(err));
+    .sync()
+    .then(() => console.log('Database synced'))
+    .catch(err => console.log(err));
 
     // Spin up server
 const PORT = process.env.DB_PORT  || 4000;
 app.listen(PORT, () => {
-	console.log(`Express app running on port: ${PORT}`);
+    console.log(`Express app running on port: ${PORT}`);
 });
