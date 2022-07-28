9060250924da61cde222d50d47667370c78e2041
jorgeheag <herreraajorgeh@gmail.com>
2022-07-27 21:40:31 -0500

    upload

---
+++ models/initModels.js
@@ -0,0 +1,40 @@
+const {Users} = require('./models/users.model');
+const {Orders} = require('./models/orders.model');
+const {Products} = require('./models/products.model');
+const {Carts} = require('./models/carts.model');
+const {Categories} = require('./models/categories.model');
+const {ProductsInCart} = require('./models/productsInCart.model');
+const {ProductImgs} = require('./models/productImgs.model');
+
+const initModels = () => {
+//Unit models relations
+Users.hasMany(Orders, {foreignkey: 'userId'});
+Orders.belongsTo(Users);
+
+Users.hasOne(Orders, {foreignkey: 'carId'});
+Orders.belongsTo(Users);
+
+Users.hasMany(Products, {foreignkey: 'userId'});
+Products.belongsTo(Users);
+
+Users.hasOne(Carts, {foreignkey: 'userId'});
+Carts.belongsTo(Users);
+
+Orders.cartId.hasOne(Carts, {foreignkey: 'id'});
+Carts.belongsTo(Orders);
+
+Products.categoryId.hasOne(Categories, {foreignkey: 'id'});
+Categories.belongsTo(Products);
+
+Products.hasMany(ProductImgs, {foreignkey: 'productId'});
+ProductImgs.belongsTo(Products);
+
+Products.hasOne(ProductsInCart, {foreignkey: 'productId'});
+ProductsInCart.belongsTo(Products);
+
+Carts.hasMany(ProductsInCart, {foreignkey: 'cartId'});
+ProductsInCart.belongsTo(Carts);
+//revisar las relaciones
+};
+
+module.exports = { initModels };
--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const { app } = require('./app');
 
+const { initModels } = require('./models/initModels')
+
 //Models
-const {Users} = require('./models/users.model');
-const {Orders} = require('./models/orders.model');
-const {Products} = require('./models/products.model');
-const {Carts} = require('./models/carts.model');
-const {Categories} = require('./models/categories.model');
-const {ProductsInCart} = require('./models/productsInCart.model');
-const {ProductImgs} = require('./models/productImgs.model');
+
 //utils
 const { db } = require('./Utils/database.util');
 
@@ -15,41 +11,15 @@ const { db } = require('./Utils/database.util');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
-
+//Relations 
+initModels();
 // Database authenticated
 db
 	.authenticate()
 	.then(() => console.log('Database authenticated'))
 	.catch(err => console.log(err));
 
-//Unit models relations
-Users.hasMany(Orders, {foreignkey: 'userId'});
-Orders.belongsTo(Users);
 
-Users.hasOne(Orders, {foreignkey: 'carId'});
-Orders.belongsTo(Users);
-
-Users.hasMany(Products, {foreignkey: 'userId'});
-Products.belongsTo(Users);
-
-Users.hasOne(Carts, {foreignkey: 'userId'});
-Carts.belongsTo(Users);
-
-Orders.cartId.hasOne(Carts, {foreignkey: 'id'});
-Carts.belongsTo(Orders);
-
-Products.categoryId.hasOne(Categories, {foreignkey: 'id'});
-Categories.belongsTo(Products);
-
-Products.hasMany(ProductImgs, {foreignkey: 'productId'});
-ProductImgs.belongsTo(Products);
-
-Products.hasOne(ProductsInCart, {foreignkey: 'productId'});
-ProductsInCart.belongsTo(Products);
-
-Carts.hasMany(ProductsInCart, {foreignkey: 'cartId'});
-ProductsInCart.belongsTo(Carts);
-//revisar las relaciones
 
 // Database synced with models' relations  
 db
