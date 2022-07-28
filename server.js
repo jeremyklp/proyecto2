05f7ab1dc6750c279f8390170610a9473b0d8264
jeremy <jeremyvallejo2016@gmail.com>
2022-07-27 23:46:34 -0500

    haber si coge

--- server.js
+++ server.js
@@ -19,32 +19,6 @@ db
 	.then(() => console.log('Database authenticated'))
 	.catch(err => console.log(err));
 
-Users.hasOne(Orders, {foreignkey: 'carId'});
-Orders.belongsTo(Users);
-
-Users.hasMany(Products, {foreignkey: 'userId'});
-Products.belongsTo(Users);
-
-Users.hasOne(Carts, {foreignkey: 'userId'});
-Carts.belongsTo(Users);
-
-Orders.hasOne(Carts, {foreignkey: 'id'});
-Carts.belongsTo(Orders);
-
-Products.hasOne(Categories, {foreignkey: 'id'});
-Categories.belongsTo(Products);
-
-Products.hasMany(ProductImgs, {foreignkey: 'productId'});
-ProductImgs.belongsTo(Products);
-
-Products.hasOne(ProductsInCart, {foreignkey: 'productId'});
-ProductsInCart.belongsTo(Products);
-
-Carts.hasMany(ProductsInCart, {foreignkey: 'cartId'});
-ProductsInCart.belongsTo(Carts);
-//revisar las relaciones
-
-
 // Database synced with models' relations  
 db
 	.sync()
